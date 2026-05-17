44ef6d7b3e6861669e9719dbeecaeb3d4bd0bf80
pai-invest <invest@pureapproach.org>
2026-05-17 08:36:03 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
               {/* Live Division Logo: Primero Collection */}
               <div className="h-12 w-full mb-6 flex items-center justify-start overflow-hidden rounded bg-[#060604] border border-neutral-900/30 px-2 py-1">
                 <img 
-                  src="/primerologo1.png" 
+                  src="/primerologo1.jpg" 
                   alt="Primero Collection Logo" 
                   className="h-full w-auto object-contain"
                 />
